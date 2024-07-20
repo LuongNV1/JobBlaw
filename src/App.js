4eaab6fa6d36810cbd44575309362f5bccd9af57
Long <machvanquan02@gmail.com>
2024-07-20 23:02:16 +0700

    create router

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import Footer from '../src/components/footer/Footer';
 import Header from '../src/components/header/Header';
 import '@fontsource/inter/400.css'; /* For regular weight */
 import '@fontsource/inter/700.css'; /* For bold weight */
+import React from 'react';
+import { BrowserRouter as Route, Routes } from 'react-router-dom';
 const App = () => {
     return (
         <>
             <Header />
-            <Overview/>
+            <Routes>
+                <Route path="/*" />
+                <Route path="/profile" element={<Overview />} />
+            </Routes>
             <Footer />
         </>
     );
