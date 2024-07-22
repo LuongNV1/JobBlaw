58293361e88228224caa0254730281373c364834
longth <longth@bsscommerce.com>
2024-07-22 11:29:51 +0700

    fix bug router

--- src/App.js
+++ src/App.js
@@ -4,17 +4,18 @@ import Header from '../src/components/header/Header';
 import '@fontsource/inter/400.css'; /* For regular weight */
 import '@fontsource/inter/700.css'; /* For bold weight */
 import React from 'react';
-import { BrowserRouter as Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 const App = () => {
     return (
-        <>
+        <BrowserRouter>
             <Header />
             <Routes>
-                <Route path="/*" />
+                {/* <Route path="/" element={<Overview />} /> */}
                 <Route path="/profile" element={<Overview />} />
             </Routes>
             <Footer />
-        </>
+        </BrowserRouter>
     );
 };
 
